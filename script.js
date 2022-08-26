518c2ec57b20383c0f586d086d47f0de6339941e
Dmitriy Bakshistov <Draginixx@yandex.ru>
2022-08-26 10:58:32 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const tabsBtn = document.querySelectorAll(".bikes__tabs-item");
+const tabsItems = document.querySelectorAll(".bikes__tabs-content")
+
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+    item.addEventListener("click", function() {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+
+        if( ! currentBtn.classList.contains('active') ) {
+            tabsBtn.forEach(function(item) {
+                item.classList.remove('active');
+            });
+        
+            tabsItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+
+            currentBtn.classList.add('active');
+            currentTab.classList.add('active');
+        }
+    });
+}
+
+document.querySelector('bikes_tabs_nav-btn').click();
