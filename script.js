a63662be035f4b0139ec1c06dd139f5f22d08029
Konstantin Filyaev <uzornakovre@yandex.ru>
2022-09-20 12:46:18 +0300

    fix: style/script

--- script.js
+++ script.js
@@ -5,24 +5,22 @@ const tabsItems = document.querySelectorAll(".bikes__tabs-content")
 tabsBtn.forEach(onTabClick);
 
 function onTabClick(item) {
-    item.addEventListener("click", function() {
-        let currentBtn = item;
-        let tabId = currentBtn.getAttribute("data-tab");
-        let currentTab = document.querySelector(tabId);
+  item.addEventListener("click", function() {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
 
-        if( ! currentBtn.classList.contains('bikes__tabs-item_active') ) {
-            tabsBtn.forEach(function(item) {
-                item.classList.remove('bikes__tabs-item_active');
-            });
+    if( ! currentBtn.classList.contains('bikes__tabs-item_active') ) {
+      tabsBtn.forEach(function(item) {
+          item.classList.remove('bikes__tabs-item_active');
+      });
 
-            tabsItems.forEach(function(item) {
-                item.classList.remove('bikes__tabs-content_active');
-            });
+      tabsItems.forEach(function(item) {
+          item.classList.remove('bikes__tabs-content_active');
+      });
 
-            currentBtn.classList.add('bikes__tabs-item_active');
-            currentTab.classList.add('bikes__tabs-content_active');
-        }
-    });
+      currentBtn.classList.add('bikes__tabs-item_active');
+      currentTab.classList.add('bikes__tabs-content_active');
+    }
+  });
 }
-
-// document.querySelector('bikes_tabs_nav-btn').click();
